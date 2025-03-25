48cf96f2a2d1c2f14b64fd2006c15832080b0b4d
Badi1298 <serbandavid83@yahoo.com>
2025-03-25 12:51:55 +0200

    feat: update NextSection button layout and styling for improved alignment and size

--- src/components/NextSection.vue
+++ src/components/NextSection.vue
@@ -1,9 +1,9 @@
 <template>
-	<button class="flex justify-center items-center text-2xl gap-x-3 px-10 py-3 z-20 text-white">
-		<span>Next section</span>
+	<button class="flex justify-end items-center text-base gap-x-4 pl-8 pr-4 py-2 z-20 text-white">
+		<span class="">Next section</span>
 		<img
 			src="/chevron-right-white.png"
-			class="w-[50px] h-auto"
+			class="w-[35px] h-auto"
 		/>
 	</button>
 </template>
