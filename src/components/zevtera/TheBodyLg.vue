cc620393ad0201c0ccf1fcb7c059b158755b4cbf
Badi1298 <serbandavid83@yahoo.com>
2025-03-25 16:49:57 +0200

    feat: add TheBodyLg component and update Zevtera layout for improved structure and readability

---
+++ src/components/zevtera/TheBodyLg.vue
@@ -0,0 +1,5 @@
+<template>
+	<p class="text-sm font-uni-grotesk leading-tight">
+		<slot></slot>
+	</p>
+</template>
--- src/pages/zevtera/Zevtera.vue
+++ src/pages/zevtera/Zevtera.vue
@@ -1,101 +1,69 @@
 <template>
-	<div class="grid grid-cols-1 grid-rows-1 pb-6 h-full">
-		<div class="flex flex-col justify-center">
-			<div class="flex">
-				<section class="flex flex-col pt-24 pr-3">
-					<div class="relative">
-						<VLazyImage
-							:src="MabelioBullet"
-							alt="Mabelio Bullet"
-							class="w-[662px]"
-						/>
-						<VLazyImage
-							:src="StrikeFast"
-							alt="Strike Fast"
-							class="absolute bottom-0 right-16 w-[278px] h-auto object-cover"
-						/>
-					</div>
-					<div class="px-24 pt-7">
-						<h1 class="relative text-[40px] font-bold leading-tight text-charcoal font-effra">
-							Allows the reduction of the number of<br />
-							agents<sup>2</sup>, thanks to its expanded spectrum<br />
-							of antimicrobial activity<sup>3</sup>
-						</h1>
-						<p class="font-uni-grotesk text-xl text-charcoal font-normal mt-9">
-							ZEVTERA®/MABELIO® is indicated for the treatment of the following infections in term neonates,<br />
-							infants, children, adolescents and adults:<sup>4</sup>
-						</p>
-						<ul>
-							<li class="flex items-center mt-4 gap-x-5">
-								<img
-									src="/list-polygon.png"
-									alt="List Polygon"
-									class="w-5 h-auto"
+	<div class="grid grid-cols-1 grid-rows-1 pb-6 h-full pr-14">
+		<div class="flex flex-col justify-start">
+			<section class="relative">
+				<VLazyImage
+					:src="MabelioBullet"
+					alt="Mabelio Bullet"
+					class="w-[662px]"
+				/>
+				<VLazyImage
+					:src="StrikeFast"
+					alt="Strike Fast"
+					class="absolute bottom-14 -right-5 w-[278px] h-auto object-cover"
+				/>
+			</section>
+			<section class="pl-14 pt-7">
+				<the-h1 class="relative !text-[30px] text-charcoal">
+					Allows the reduction of the number of agents<sup class="text-[60%] -top-[0.45em]">2</sup>,<br />
+					thanks to its expanded spectrum of antimicrobial activity<sup class="text-[60%] -top-[0.45em]">3</sup>
+				</the-h1>
+				<div class="flex mt-7">
+					<the-body-lg class="text-charcoal">
+						ZEVTERA®/MABELIO® is indicated for the treatment of<br />
+						the following infections in term neonates, infants, children,<br />
+						adolescents and adults:<sup class="text-[60%] -top-[0.8em]">4</sup>
+					</the-body-lg>
+					<ul class="ml-11">
+						<li class="flex gap-x-3">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="16"
+								height="12"
+								viewBox="0 0 16 12"
+								fill="none"
+								class="translate-y-0.5"
+							>
+								<path
+									d="M15.712 5.88465L0.928005 11.575L0.928005 0.194269L15.712 5.88465Z"
+									fill="#009656"
 								/>
-								<p class="font-uni-grotesk text-xl text-charcoal font-normal">
-									Hospital-acquired pneumonia (HAP), excluding ventilator-associated pneumonia (VAP)
-								</p>
-							</li>
-							<li class="flex items-center mt-4 gap-x-5">
-								<img
-									src="/list-polygon.png"
-									alt="List Polygon"
-									class="w-5 h-auto"
+							</svg>
+							<the-body-lg class="text-charcoal">
+								Hospital-acquired pneumonia (HAP), excluding ventilator-<br />associated pneumonia (VAP)
+							</the-body-lg>
+						</li>
+						<li class="flex gap-x-3 mt-2 items-center">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="16"
+								height="12"
+								viewBox="0 0 16 12"
+								fill="none"
+							>
+								<path
+									d="M15.712 5.88465L0.928005 11.575L0.928005 0.194269L15.712 5.88465Z"
+									fill="#009656"
 								/>
-								<p class="font-uni-grotesk text-xl text-charcoal font-normal">Community-acquired pneumonia (CAP)</p>
-							</li>
-						</ul>
-					</div>
-				</section>
-				<!-- <section class="pt-36 -translate-x-1">
-					<h2 class="text-[32px] font-bold text-charcoal font-effra">I'm interested in:</h2>
-					<card
-						title="Efficacy"
-						subtitle="Demonstrated efficacy with clinical<br /> efficacy from Days 3-4, with more pathogens covered<sup>5-8</sup>"
-						button-class="bg-[#009656] text-white"
-						button-text-class="max-h-4"
-						discover-more-path="/zevtera/efficacy"
-						class="mt-9 font-uni-grotesk max-w-[424px] pt-7"
-					>
-						<img
-							src="/chart-green.png"
-							alt="Chart Dark Green"
-							class="w-[106px] h-auto"
-						/>
-					</card>
-					<card
-						title="Safety"
-						subtitle="ZEVTERA®/MABELIO® is<br /> generally well tolerated<sup>9</sup>"
-						button-class="bg-[#FFA94D] text-black"
-						button-text-class="max-h-4"
-						discover-more-path="/zevtera/safety"
-						class="mt-11 font-uni-grotesk max-w-[424px] pt-9"
-					>
-						<img
-							src="/shield-orange.png"
-							alt="Shiled Orange"
-							class="w-[106px] h-auto"
-						/>
-					</card>
-					<card
-						title="Dosing"
-						subtitle="Ease-of-use dosing<sup>4</sup>"
-						button-class="bg-[#FCC100] text-black"
-						button-text-class="max-h-4"
-						discover-more-path="/zevtera/dosing"
-						class="mt-11 font-uni-grotesk max-w-[424px] pt-9"
-					>
-						<img
-							src="/bottle-golden.png"
-							alt="Dosing Golden"
-							class="w-[106px] h-auto"
-						/>
-					</card>
-				</section> -->
-			</div>
+							</svg>
+							<the-body-lg class="text-charcoal">Community-acquired pneumonia (CAP)</the-body-lg>
+						</li>
+					</ul>
+				</div>
+			</section>
 		</div>
 		<footer>
-			<the-footer class="transition-all duration-300 ml-24 font-uni-grotesk"
+			<the-footer class="transition-all duration-300 ml-14 font-uni-grotesk"
 				>Zevtera® is the trademark in UK, Germany, Spain, Denmark, Norway, Sweden and Finland; Mabelio® is the trademark in France and Italy.
 				Commercialised by ADVANZ PHARMA® in Germany, Spain, Italy,<br />
 				France, and UK. Zevtera® / Mabelio ® is licensed to ADVANZ PHARMA by Basilea Pharmaceutica International AG.<br />©2025 ADVANZ
@@ -113,6 +81,12 @@ import StrikeFast from '/strike-fast.png';
 import MabelioBullet from '/mabelio-bullet.png';
 
 import VLazyImage from 'v-lazy-image';
+
+import TheH1 from '@/components/zevtera/TheH1.vue';
+import TheH2 from '@/components/zevtera/TheH2.vue';
+import TheBody from '@/components/zevtera/TheBody.vue';
+import TheBodyLg from '@/components/zevtera/TheBodyLg.vue';
+import TheFootnotes from '@/components/zevtera/TheFootnotes.vue';
 
 import Card from '@/components/Card.vue';
 import TheFooter from '@/components/TheFooter.vue';
